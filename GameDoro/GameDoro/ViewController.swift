8e85802867d81996b484cb01542889598cb357bc
Nikita Semenov <castlelecs@gmail.com>
2022-04-24 13:13:13 +0300

    initial commit

---
+++ GameDoro/GameDoro/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GameDoro
+//
+//  Created by Nikita Semenov on 24.04.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
